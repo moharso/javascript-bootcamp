e4b6049c5f49da90841994cbe69676758083c1f2
Karolina <karolinamasharo@gmail.com>
2023-06-08 17:07:31 +0300

    Add second function

--- script.js
+++ script.js
@@ -12,8 +12,16 @@ const checkNumber = (number) => {
 console.log(checkNumber(0));
 
 // Second function: Function takes at least 3 arguments.
+const evenOrOdd = (a, b, c) => {
+    const averageSum = (a + b + c) / 3 
+    if (averageSum % 2 == 0) {
+        return "Even number"
+  } else {
+      return "Odd number"
+  }
+}
 
-
+  console.log(evenOrOdd(3, 3, 3))
 
 //Third function: Use Object key & value pairs in it.
 const shoppingList = (item1, item2) => {
