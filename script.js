5f0389af525bb7d925e7bb87bc0cb62bff64b960
Karolina <karolinamasharo@gmail.com>
2023-06-13 11:05:06 +0300

    Add homework task

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-// First function: If statements
-const checkNumber = (number) => {
-  if (number > 0) {
-    return "Positive";
-  } else if (number < 0) {
-    return "Negative";
-  } else {
-    return "Zero";
+// Write a sorting algorithm of your choice with using only for loop or forEach. 
+const myArr = [-6, 50, 4, 86, 8, 1, -2, 33]
+
+function sortArr(arr) {
+    arr.forEach((elem, index) => {
+      for (let i = index + 1; i < arr.length; i++) {
+        if (arr[i] < arr[index]) {
+          // Swap elements if they are in the wrong order
+          [arr[index], arr[i]] = [arr[i], arr[index]];
+        }
+      }
+    });
+  
+    return arr;
   }
-};
 
-console.log(checkNumber(0));
-
-// Second function: Function takes at least 3 arguments.
-const evenOrOdd = (a, b, c) => {
-    const averageSum = (a + b + c) / 3 
-    if (averageSum % 2 == 0) {
-        return "Even number"
-  } else {
-      return "Odd number"
-  }
-}
-
-  console.log(evenOrOdd(3, 3, 3))
-
-//Third function: Use Object key & value pairs in it.
-const shoppingList = (item1, item2) => {
-  const list = {
-    item1: item1,
-    item2: item2,
-  };
-  return `Need to buy ${list.item1} and ${list.item2}`;
-};
-
-console.log(shoppingList("milk", "bread"));
+  console.log(sortArr(myArr)) //Output: [ -6, -2,  1,  4, 8, 33, 50, 86]
